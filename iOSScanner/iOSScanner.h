fdec8074607fcb356d3e5fd27c4c83a6e4081bcf
Liew Jun Tung <juntung@netvirta.com>
2018-11-01 13:32:44 +0800

    Initial Commit

---
+++ iOSScanner/iOSScanner.h
@@ -0,0 +1,19 @@
+//
+//  iOSScanner.h
+//  iOSScanner
+//
+//  Created by Jun Tung Liew on 1/11/18.
+//  Copyright © 2018 Netvirta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for iOSScanner.
+FOUNDATION_EXPORT double iOSScannerVersionNumber;
+
+//! Project version string for iOSScanner.
+FOUNDATION_EXPORT const unsigned char iOSScannerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <iOSScanner/PublicHeader.h>
+
+
