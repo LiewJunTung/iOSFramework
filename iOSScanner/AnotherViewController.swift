1b5e30493af340a9fccd1b2a1bc58936a7a31e7d
Liew Jun Tung <juntung@netvirta.com>
2018-11-01 15:06:31 +0800

    done with making it work with testing app

---
+++ iOSScanner/AnotherViewController.swift
@@ -0,0 +1,31 @@
+//
+//  AnotherViewController.swift
+//  iOSScanner
+//
+//  Created by Jun Tung Liew on 1/11/18.
+//  Copyright © 2018 Netvirta. All rights reserved.
+//
+
+import UIKit
+
+public class AnotherViewController: UIViewController {
+    public static let STORYBOARD_ID = "Another"
+
+    override public func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ iOSScanner/XViewController.m
@@ -0,0 +1,32 @@
+//
+//  XViewController.m
+//  iOSScanner
+//
+//  Created by Jun Tung Liew on 1/11/18.
+//  Copyright © 2018 Netvirta. All rights reserved.
+//
+
+#import "XViewController.h"
+
+@interface XViewController ()
+
+@end
+
+@implementation XViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
